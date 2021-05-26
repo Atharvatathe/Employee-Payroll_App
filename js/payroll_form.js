2a815eae940e88e8ba0647593b84c76d78dc1881
Atharvatathe <atharva.tathe55@gmail.com>
2021-05-26 17:26:33 +0530

    Added:Reset the form on clicking Reset button

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -94,3 +94,33 @@ window.addEventListener('DOMContentLoaded', (event) => {
         localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList))
     }
   
+    /* UC13-Reset the form on clicking reset button */
+
+    const resetForm = () => {
+        setValue('#name', '');
+        unsetSelectedValues('[name=profile]');
+        unsetSelectedValues('[name=gender]');
+        unsetSelectedValues('[name=department]');
+        setValue('#salary', '');
+        setValue('#notes', '');
+        setValue('#day', '1');
+        setValue('#month', 'January');
+        setValue('#year', '2020');
+    }
+    
+    const unsetSelectedValues = (propertyValue) => {
+        let allItems = document.querySelectorAll(propertyValue);
+        allItems.forEach(item => {
+            item.checked = false;
+        });
+    }
+    
+    const setTextValue = (id, value) => {
+        const element = document.querySelector(id);
+        element.textContent = value;
+    }
+    
+    const setValue = (id, value) => {
+        const element = document.querySelector(id);
+        element.textContent = value;
+    }
