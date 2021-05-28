0acaa359c6c06ff8751e77ea776151784fab489e
Atharvatathe <atharva.tathe55@gmail.com>
2021-05-28 19:58:57 +0530

    Added: Display all Employee Payroll Details from json object

--- js/home.js
+++ js/home.js
@@ -5,22 +5,69 @@ window.addEventListener('DOMContentLoaded', (event) => {
 createInnerHtml = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>" +
                         "<th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
-    const innerHtml = `${headerHtml}
-        <tr>
-          <td><img class="progile" src="../assets/profile-images/Ellipse -2.png" alt=""></td>
-          <td>Atharva</td>
-          <td>Male</td>
-          <td>
-            <div class="dept-label">HR</div>
-            <div class="dept-label">Finance</div></td>
-          <td>400000</td>
-          <td>2 Nov 2020</td>
-          <td><img id="1" onclick="remove(this)" src="../assets/icons/delete-black-18dp.svg" alt="delete">
-            <img id="1" onclick="update(this)" src="../assets/icons/create-black-18dp.svg" alt="edit">
-          </td>    
-        </tr>
-    `;
-    document.querySelector('#table-display').innerHTML = innerHtml;
+    //set innerHtml                   
+    let innerHtml = `${headerHtml}`;
+    let employeePayrollList = createEmployeePayrollJSON();
     
-    
+    //for loop for printing multiple employee
+    for(const empPayrollData of employeePayrollList) {
+      innerHtml = `${innerHtml}
+      <tr>
+        <td><img class="progile" src="${empPayrollData._profilePic}" alt=""></td>
+        <td>${empPayrollData._name}</td>
+        <td>${empPayrollData._gender}</td>
+        <td>${getDeptHtml(empPayrollData._department)}</td>
+        <td>${empPayrollData._salary}</td>
+        <td>${empPayrollData._startDate}</td>
+        <td>
+          <img id="${empPayrollData._id}" onclick="remove(this)" src="../assets/icons/delete-black-18dp.svg" alt="delete">
+          <img id="${empPayrollData._id}" onclick="update(this)" src="../assets/icons/create-black-18dp.svg" alt="edit">
+        </td>    
+      </tr>
+  `;
+    }
+  document.querySelector('#table-display').innerHTML = innerHtml;
+  
+}
+
+
+
+//JSON objects
+const createEmployeePayrollJSON = () => {
+  let empPayrollListLocal = [
+      {
+          _name: 'Atharva',
+          _gender: 'Male',
+          _department: [
+              'Engineering', 'Finance'
+          ],
+          _salary: '800000',
+          _startDate: '24 MAY 2018',
+          _note: '',
+          _id: new Date().getTime(),
+          _profilePic: '../assets/profile-images/Ellipse -2.png'
+      },
+      {
+          _name: 'Disha',
+          _gender: 'Female',
+          _department: [
+              'HR'
+          ],
+          _salary: '500000',
+          _startDate: '18 Sep 2019',
+          _note: '',
+          _id: new Date().getTime(),
+          _profilePic: '../assets/profile-images/Ellipse -1.png'
+      }
+  ];
+  return empPayrollListLocal;
+}
+
+//To print Multiple Department without showing 'undefined'.
+const getDeptHtml = (deptList) => {
+  let deptHtml = '';
+  for (const dept of deptList) {
+      deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+  }
+  return deptHtml;
 }
